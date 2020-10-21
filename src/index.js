93479d613353db0b04cd8a3d2547d14ce120d9bd
Krishna <7krishna7dhakal7@gmail.com>
2020-10-21 23:12:08 +0545

    biswas code evolution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from 'react-dom';
+
+import store from './store/store'
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
